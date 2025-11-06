4518c98df4d8e0fbbafc15b02ab787f46d7224f5
drakowkq <drakowkq@example.com>
2025-11-06 19:59:28 +0700

    update lab4

--- task1/task1_4/task_a.c
+++ task1/task1_4/task_a.c
@@ -1,42 +1,41 @@
-// a_print_cancel.c
-#define _XOPEN_SOURCE 700
-#include <pthread.h>
 #include <stdio.h>
-#include <unistd.h>   // sleep
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
 
-void* worker(void* arg) {
-	char* string = "Stroka";
-	int counter = 0;
-	while (1) {
-		counter++;
-		printf("%s num. %d\n", string, counter);
-	}
-	return NULL;
+void* thread_func(void* arg) {
+    while (1) {
+        printf("Thread is running...\n");
+        sleep(1);
+    }
+    return NULL;
 }
 
-int main(void) {
-    pthread_t tid;
-    int err;
-    if (pthread_create(&tid, NULL, worker, NULL) != 0) {
-        perror("pthread_create");
+int main() {
+    pthread_t thread;
+
+    int err = pthread_create(&thread, NULL, thread_func, NULL);
+	if (err) {
+	    printf("main: pthread_create() failed: %s\n", strerror(err));
+		return 1;
+	}
+
+    sleep(5);
+
+    err = pthread_cancel(thread);
+    if (err) {
+        printf("main: pthread_cancel() failed: %s\n", strerror(err));
         return 1;
     }
 
-    sleep(3); // даём потоку поработать ~3 секунды
-    puts("[main] requesting cancellation...");
-    pthread_cancel(tid);
-
-    void* ret = NULL;
-    if (pthread_join(tid, &ret) != 0) {
-        perror("pthread_join");
+    err = pthread_join(thread, NULL);
+    if (err) {
+        printf("main: pthread_join() failed: %s\n", strerror(err));
         return 1;
     }
 
-    if (ret == PTHREAD_CANCELED) {
-        puts("[main] thread was canceled (PTHREAD_CANCELED).");
-    } else {
-        puts("[main] thread exited normally.");
-    }
+    printf("Thread has been cancelled.\n");
+
     return 0;
 }
